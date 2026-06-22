08caea588714c9966720d49745040bffced850e5
Emmanuel lelo <emmanuelmassawe200@gmail.com>
2026-06-22 18:01:55 +0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -115,4 +115,4 @@ def predict(request: PredictionRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True)
